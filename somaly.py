0618a2e790a76c12db6d78b0fd2c39664617a941
NonameIncognitos <anosvoldigoad492@gmail.com>
2024-01-29 12:57:56 +0600

    cpp

--- somaly.py
+++ somaly.py
@@ -1,27 +1,32 @@
 import requests
 import time
 
+
 API_URL = 'https://api.telegram.org/bot'
-BOT_TOKEN = '6654614422:AAG8nK3ULsuOZlanJ33DTiOJ4E_Ggto_cEM'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = ''
+ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
 
-TEXT = 'Шаг ближе к backend'
-
-MAX_COUNTER = 100
-
-offset = 0
+offset = -2
 counter = 0
-chat_id: int
+cat_response: requests.Response
+cat_link: str
 
-while counter < MAX_COUNTER:
-    print('attempt', counter)
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+
+while counter < 100:
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-    
-    time.sleep(0.5)
+    time.sleep(1)
     counter += 1
